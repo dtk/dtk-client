2e12a7c79b9f1022b153bc1084928de6175e7700
Aldin Milanovic <aldin@atlantbh.com>
2016-08-12 15:21:44 +0200

    Fix for ssh command

--- lib/client/operation/service/ssh.rb
+++ lib/client/operation/service/ssh.rb
@@ -53,9 +53,9 @@ module DTK::Client
       end
 
       def self.get_node_info_for_ssh_login(node_name, service_instance)
-        response = rest_get("#{BaseRoute}/#{service_instance}/nodes")
+        response = rest_get("#{BaseRoute}/#{service_instance}/info")
 
-        unless node_info = (response.data||[]).find{ |node| node_name == node['display_name'] }
+        unless node_info = response.data(:nodes).find{ |node| node_name == node['display_name'] }
           raise Error::Usage, "Cannot find info about node with id '#{node_id}'"
         end
 
