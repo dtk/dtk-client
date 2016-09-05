01a546a53930c2e1687d369f43d9511e4d020668
Aldin Milanovic <aldin@atlantbh.com>
2016-09-05 14:10:45 +0200

    Display versions in dtk module list

--- lib/client/operation/module/list.rb
+++ lib/client/operation/module/list.rb
@@ -22,7 +22,7 @@ module DTK::Client
         wrap_operation(args) do |_args|
           # defaults
           query_string_hash = QueryStringHash.new(
-            :detail_to_include => ['remotes']
+            :detail_to_include => ['remotes', 'versions']
           )
           rest_get("#{BaseRoute}/list", query_string_hash)
         end.set_render_as_table!
