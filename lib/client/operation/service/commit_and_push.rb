ef5f2b1fba96aa00e9980a17b89442b7e1bff14a
Rich Pelavin <rich.reactor8.com>
2016-09-14 03:38:19 +0000

    DTK-2663: putting in rough cut of processing info returned when execute 'dtk service push'

--- lib/client/operation/service/commit_and_push.rb
+++ lib/client/operation/service/commit_and_push.rb
@@ -35,13 +35,7 @@ module DTK::Client
           commit_sha = response.required(:head_sha)
 
           response = rest_post("#{BaseRoute}/#{service_instance}/update_from_repo", :commit_sha => commit_sha)
-          if !response.data(:warning_msgs).empty?
-            response.data(:warning_msgs).each { |warning_msg| OsUtil.print_warning(warning_msg) }
-          end
-
-          if response.data(:repo_updated)
-            # TODO: code to pull repo info
-          end
+          process_response(response)
           nil
         end
       end
@@ -55,8 +49,49 @@ module DTK::Client
       def self.head_commit_sha(service_instance)
         raise Error, "Need to write"
       end
+
+      def self.process_response(response)
+        print_msgs_of_type(:error_msgs, response)
+        print_msgs_of_type(:warning_msgs, response)
+        print_msgs_of_type(:info_msgs, response)
+
+        process_backup_files(response)
+        process_semantic_diffs(response)
+      end
+
+      def self.process_backup_files(response)
+        backup_files = response.data(:backup_files) || {}
+        return if backup_files.empty?
+        pp 'TODO: DTK-2663; write code that saves each file under the service module, commits it alomng with an update if needed to .gitignore'
+        pp 'DEBUG_PRINT_of_info'
+        backup_files.each_pair do |file_path, content|
+          pp [:file_path, file_path]
+          STDOUT << content
+          pp '----'
+        end
+      end
+
+      def self.process_semantic_diffs(response)
+        semantic_diffs = response.data(:semantic_diffs) || {}
+        return if semantic_diffs.empty?
+        pp 'TODO: DTK-2663; cleanup so pretty printed'
+        OsUtil.print('Diffs that were pushed:')
+        # TODO: get rid of use of STDOUT
+        STDOUT << hash_to_yaml(semantic_diffs)
+      end
+
+      PRINT_FN = {
+        :info_msgs    => lambda { |msg| OsUtil.print_info(msg) },
+        :warning_msgs => lambda { |msg| OsUtil.print_warning(msg) },
+        :error_msgs => lambda { |msg| OsUtil.print_error(msg) }
+      }
+      def self.print_msgs_of_type(msg_type, response)
+        msgs = response.data(msg_type) || []
+        unless msgs.empty?
+          print_fn = PRINT_FN[msg_type]
+          msgs.each { |msg| print_fn.call(msg) }
+        end
+      end
     end
   end
 end
-
-
