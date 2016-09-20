7cf4ab9eada12f3d88474c40d1aa8f26884ab188
Rich Pelavin <rich.reactor8.com>
2016-09-20 22:15:53 +0000

    DTK-2667: put 'dtk module push' command back in so can work on DTK-2667

--- lib/cli/command/module.rb
+++ lib/cli/command/module.rb
@@ -21,9 +21,7 @@ module DTK::Client
       module Module 
         include Command::Mixin
 
-        # TODO: removed push command until fixed by jira: DTK-2667
-        # ALL_SUBCOMMANDS = ['install', 'list', 'list-assemblies', 'push', 'uninstall', 'clone']
-         ALL_SUBCOMMANDS = ['install', 'list', 'list-assemblies', 'uninstall', 'clone']
+        ALL_SUBCOMMANDS = ['install', 'list', 'list-assemblies', 'push', 'uninstall', 'clone']
         command_def :desc => 'Subcommands for interacting with DTK modules'
         ALL_SUBCOMMANDS.each { |subcommand| require_relative("module/#{subcommand.gsub(/-/,'_')}") } 
       end
