baa8b6271bf96eeb12306a439d82fbea6c2837d8
Aldin Milanovic <aldin@atlantbh.com>
2016-12-02 16:24:29 +0100

    Added push-dtkn command placeholder to dtk-client; need to implement logic for transform to

--- lib/cli/command/module/push_dtkn.rb
+++ lib/cli/command/module/push_dtkn.rb
@@ -16,15 +16,18 @@
 # limitations under the License.
 #
 module DTK::Client
-  module CLI
-    module Command
-      module Module 
-        include Command::Mixin
-
-        ALL_SUBCOMMANDS = ['install', 'list', 'list-assemblies', 'push', 'uninstall', 'clone', 'list-remotes', 'push-dtkn']
-        command_def :desc => 'Subcommands for interacting with DTK modules'
-        ALL_SUBCOMMANDS.each { |subcommand| require_relative("module/#{subcommand.gsub(/-/,'_')}") } 
+  module CLI::Command
+    module Module 
+      subcommand_def 'push-dtkn' do |c|
+        command_body c, 'push-dtkn', 'Push content from client module directory to repo manager' do |sc|
+          sc.flag Token.directory_path, :desc => 'Absolute or relative path to module directory containing updates to push; not need if in the module directory'
+          sc.action do |_global_options, options, _args|
+            module_ref = module_ref_in_options_or_context(options)
+            Operation::Module.push_dtkn(:module_ref => module_ref, :base_dsl_file_obj => @base_dsl_file_obj)
+          end
+        end
       end
+
     end
   end
 end
---
+++ lib/client/operation/module/push_dtkn.rb
@@ -0,0 +1,69 @@
+#
+# Copyright (C) 2010-2016 dtk contributors
+#
+# This file is part of the dtk project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module DTK::Client
+  class Operation::Module
+    class PushDtkn < self
+      attr_reader :version, :module_ref, :target_repo_dir
+      def initialize(catalog, module_ref, directory_path, version)
+        @catalog          = catalog
+        @module_ref       = module_ref
+        @directory_path   = directory_path
+        @target_repo_dir  = directory_path || OsUtil.current_dir
+        @version          = version # if nil wil be dynamically updated
+      end
+      private :initialize
+
+      def self.execute(args = Args.new)
+        wrap_operation(args) do |args|
+          module_ref     = args.required(:module_ref)
+          version        = args[:version]
+          directory_path = args[:directory_path]
+
+          # will create different classes for different catalog types when we add support for them
+          new('dtkn', module_ref, directory_path, version).push_dtkn
+        end
+      end
+      
+      def push_dtkn
+        # TODO: DTK-2765: not sure if we need module to exist on server to do push-dtkn
+        unless module_exists?(@module_ref, :type => :common_module)
+          raise Error::Usage, "Module #{@module_ref.print_form} does not exist on server"
+        end
+
+        query_string_hash = QueryStringHash.new(
+          :module_name => @module_ref.module_name,
+          :namespace   => @module_ref.namespace,
+          :rsa_pub_key => SSHUtil.rsa_pub_key_content,
+          :version?    => @version
+        )
+
+        remote_module_info = rest_get "#{BaseRoute}/remote_module_info", query_string_hash
+
+        @version ||= remote_module_info.required(:version)
+
+        # LoadSource.fetch_transform_and_merge(remote_module_info, self)
+        # call method that will transform to repo manager readable form
+
+        {:target_repo_dir => @target_repo_dir}
+      end
+
+    end
+  end
+end
+
+
